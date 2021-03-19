52fc14a5a36412e549c6ec1c6f9c0752b1bc4f9e
kailey.smith <kaileysmith2021@u.northwestern.edu>
2021-03-19 15:36:11 -0500

    Edited the map_metadata so its not freaking hard coded

--- multi_turtlebot_slam/src/map_metadata.cpp
+++ multi_turtlebot_slam/src/map_metadata.cpp
@@ -56,115 +56,123 @@ int main(int argc, char * argv[])
 
   while (ros::ok())
   {
-      nav_msgs::OccupancyGrid new_tb3_0_map, new_tb3_1_map;
-      new_tb3_0_map.header.frame_id = "new_tb3_0_map";
-      new_tb3_0_map.info.resolution = 0.05;
-      new_tb3_0_map.info.origin.position.x =  -10.0;
-      new_tb3_0_map.info.origin.position.y = -10.0;
-      new_tb3_0_map.info.origin.position.z = 0.0;
-      new_tb3_0_map.info.origin.orientation.w = 0.0;
+    if ( (map0_msg.data.size() != 0) || (map1_msg.data.size() != 0) || map0_msg.info.origin.position.x !=0)
+    {
+        nav_msgs::OccupancyGrid new_tb3_0_map, new_tb3_1_map;
+        new_tb3_0_map.header.frame_id = "new_tb3_0_map";
+        new_tb3_0_map.info.resolution = 0.05;
+        new_tb3_0_map.info.origin.position.x =  -10.0;
+        new_tb3_0_map.info.origin.position.y = -10.0;
+        new_tb3_0_map.info.origin.position.z = 0.0;
+        new_tb3_0_map.info.origin.orientation.w = 0.0;
 
-      new_tb3_1_map.header.frame_id = "new_tb3_1_map";
-      new_tb3_1_map.info.resolution = 0.05;
-      new_tb3_1_map.info.origin.position.x =  new_tb3_0_map.info.origin.position.x;
-      new_tb3_1_map.info.origin.position.y = new_tb3_0_map.info.origin.position.y;
-      new_tb3_1_map.info.origin.position.z = new_tb3_0_map.info.origin.position.z;
-      new_tb3_1_map.info.origin.orientation.w = new_tb3_0_map.info.origin.orientation.w;
+        new_tb3_1_map.header.frame_id = "new_tb3_1_map";
+        new_tb3_1_map.info.resolution = 0.05;
+        new_tb3_1_map.info.origin.position.x =  new_tb3_0_map.info.origin.position.x;
+        new_tb3_1_map.info.origin.position.y = new_tb3_0_map.info.origin.position.y;
+        new_tb3_1_map.info.origin.position.z = new_tb3_0_map.info.origin.position.z;
+        new_tb3_1_map.info.origin.orientation.w = new_tb3_0_map.info.origin.orientation.w;
 
-      const size_t width_ = 384;
-      const size_t height_ = 384;
-      const size_t bottom0_width_ = 51; // Space between the bottom of the big map and the local slam0 map
-      const size_t bottom1_width_ = 299; // Space between the bottom of the big map and the local slam1 map
-      new_tb3_0_map.info.width = width_;
-      new_tb3_0_map.info.height = height_;
-      new_tb3_1_map.info.width = new_tb3_0_map.info.width;
-      new_tb3_1_map.info.height = new_tb3_0_map.info.height;
+        const size_t width_ = 384;
+        const size_t height_ = 384;
 
-      /////////////////////////////////////////////////////////////////////////////
-      // For the frist turtlebot (tb3_0)
-      ////////////////////////////////////////////////////////////////////////////
-      // Map starts loading in from origin in bottom right
-      // From the origin, the row components corresponds with width (x-dir which is actually up)
-      // Create empty space on rhs of map (total width and 122 pixels 'high' (to the left))
+        const size_t bottom0_width_ = (map0_msg.info.origin.position.x - new_tb3_0_map.info.origin.position.x) / new_tb3_0_map.info.resolution;
+        const size_t bottom1_width_ = (map1_msg.info.origin.position.x - new_tb3_1_map.info.origin.position.x) / new_tb3_1_map.info.resolution;
 
-      int c0 = 0; // start a counter for map0
+        new_tb3_0_map.info.width = width_;
+        new_tb3_0_map.info.height = height_;
+        new_tb3_1_map.info.width = new_tb3_0_map.info.width;
+        new_tb3_1_map.info.height = new_tb3_0_map.info.height;
 
-      for (int i=0;  i < new_tb3_0_map.info.width * 100; i++) // orginally 123
-      {
-        new_tb3_0_map.data.push_back(-1);
-      }
+        /////////////////////////////////////////////////////////////////////////////
+        // For the frist turtlebot (tb3_0)
+        ////////////////////////////////////////////////////////////////////////////
+        // Map starts loading in from origin in bottom right
+        // From the origin, the row components corresponds with width (x-dir which is actually up)
+        // Create empty space on rhs of map (total width and 122 pixels 'high' (to the left))
 
-      for (int item_counter=0; item_counter < slam0_map.info.height; item_counter++)
-      {
+        int c0 = 0; // start a counter for map0
 
-        for (int q=0; q < bottom0_width_; q++)
+        // This fills in the right hand side of the new map to the slam map
+        for (int i=0;  i < new_tb3_0_map.info.width * 123; i++) // orginally 123
         {
           new_tb3_0_map.data.push_back(-1);
         }
 
-        for (int a = 0; a < slam0_map.info.width; a++)
+        // For the new larger map, fill in the spaces next to the slam map with -1s
+        for (int item_counter=0; item_counter < slam0_map.info.height; item_counter++)
         {
-          new_tb3_0_map.data.push_back(slam0_map.data[c0]);
-          c0++;
-        }
 
-        for (int u=0; u < (new_tb3_0_map.info.width - slam0_map.info.width - bottom0_width_); u++)
+          for (int q=0; q < bottom0_width_; q++)
+          {
+            new_tb3_0_map.data.push_back(-1);
+          }
+
+          for (int a = 0; a < slam0_map.info.width; a++)
+          {
+            new_tb3_0_map.data.push_back(slam0_map.data[c0]);
+            c0++;
+          }
+
+          for (int u=0; u < (new_tb3_0_map.info.width - slam0_map.info.width - bottom0_width_); u++)
+          {
+            new_tb3_0_map.data.push_back(-1);
+          }
+        } 
+
+        // Fill in the left hand side of the new map
+        for (int z=0;  z < ((height_ - slam0_map.info.height - 123) * new_tb3_0_map.info.width); z++)
         {
           new_tb3_0_map.data.push_back(-1);
         }
-      } 
 
-      for (int z=0;  z < ((height_ - slam0_map.info.height - 100) * new_tb3_0_map.info.width); z++)
-      {
-        new_tb3_0_map.data.push_back(-1);
-      }
+        /////////////////////////////////////////////////////////////////////////////
+        // For the second turtlebot (tb3_1)
+        ////////////////////////////////////////////////////////////////////////////
+        // Map starts loading in from origin in bottom right
+        // From the origin, the row components corresponds with width (x-dir which is actually up)
+        // Create empty space on rhs of map (total width and 122 pixels 'high' (to the left))
 
-      /////////////////////////////////////////////////////////////////////////////
-      // For the second turtlebot (tb3_1)
-      ////////////////////////////////////////////////////////////////////////////
-      // Map starts loading in from origin in bottom right
-      // From the origin, the row components corresponds with width (x-dir which is actually up)
-      // Create empty space on rhs of map (total width and 122 pixels 'high' (to the left))
+        int c1 = 0; // start a counter for map 2
 
-      int c1 = 0; // start a counter for map 2
-
-      // Fill in the empty space on the right hand side of the map
-      for (int i=0;  i < new_tb3_1_map.info.width * 113; i++)
-      {
-        new_tb3_1_map.data.push_back(-1);
-      }
-
-      // Fill in the area where the map1 is, also the area above and below that
-      for (int item_counter=0; item_counter < slam1_map.info.height; item_counter++)
-      {
-        // Space below the maps width
-        for (int q=0; q < bottom1_width_; q++)
+        // Fill in the empty space on the right hand side of the map
+        for (int i=0;  i < new_tb3_1_map.info.width * 113; i++)
         {
           new_tb3_1_map.data.push_back(-1);
         }
 
-        // Fill in the map data for the current width row
-        for (int a = 0; a < slam1_map.info.width; a++)
+        // Fill in the area where the map1 is, also the area above and below that
+        for (int item_counter=0; item_counter < slam1_map.info.height; item_counter++)
         {
-          new_tb3_1_map.data.push_back(slam1_map.data[c1]);
-          c1++;
-        }
+          // Space below the maps width
+          for (int q=0; q < bottom1_width_; q++)
+          {
+            new_tb3_1_map.data.push_back(-1);
+          }
 
-        // Fill in the space above the map
-        for (int u=0; u < (new_tb3_1_map.info.width - slam1_map.info.width - bottom1_width_); u++)
+          // Fill in the map data for the current width row
+          for (int a = 0; a < slam1_map.info.width; a++)
+          {
+            new_tb3_1_map.data.push_back(slam1_map.data[c1]);
+            c1++;
+          }
+
+          // Fill in the space above the map
+          for (int u=0; u < (new_tb3_1_map.info.width - slam1_map.info.width - bottom1_width_); u++)
+          {
+            new_tb3_1_map.data.push_back(-1);
+          }
+        } 
+
+        // Fill in the area on the left hand side of the map
+        for (int z=0;  z < ((height_ - slam1_map.info.height - 113) * new_tb3_1_map.info.width); z++)
         {
           new_tb3_1_map.data.push_back(-1);
         }
-      } 
 
-      // Fill in the area on the left hand side of the map
-      for (int z=0;  z < ((height_ - slam1_map.info.height - 113) * new_tb3_1_map.info.width); z++)
-      {
-        new_tb3_1_map.data.push_back(-1);
-      }
-
-      new_tb3_0_map_pub.publish(new_tb3_0_map);
-      new_tb3_1_map_pub.publish(new_tb3_1_map);
+        new_tb3_0_map_pub.publish(new_tb3_0_map);
+        new_tb3_1_map_pub.publish(new_tb3_1_map);
+    }
 
       ros::spinOnce();
       loop_rate.sleep();
